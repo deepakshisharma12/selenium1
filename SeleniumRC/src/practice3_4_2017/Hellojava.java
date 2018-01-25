55beafe64da1b69d26349adf69665ead315665ae
deepakshisharma12 <deepakshi.cs@gmail.com>
2018-01-25 23:21:12 +0530

    Commit added new line for frst progm.

--- SeleniumRC/src/practice3_4_2017/Hellojava.java
+++ SeleniumRC/src/practice3_4_2017/Hellojava.java
@@ -3,7 +3,7 @@ package practice3_4_2017;
 public class Hellojava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("Frst program for hello java");
 
 	}
 
