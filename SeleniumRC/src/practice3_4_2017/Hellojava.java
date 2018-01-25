ce346f29bb329781a178472f25821828babb9ef2
deepakshisharma12 <deepakshi.cs@gmail.com>
2018-01-25 23:15:57 +0530

    Commit added new lines.

--- SeleniumRC/src/practice3_4_2017/Hellojava.java
+++ SeleniumRC/src/practice3_4_2017/Hellojava.java
@@ -3,7 +3,9 @@ package practice3_4_2017;
 public class Hellojava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a,c=1,d=2;
+		a=c+d;
+		System.out.println(a);
 
 	}
 
