ef0c7bd68d312c1ee0d3b8df317f5f94f4d8fdd2
deepakshisharma12 <deepakshi.cs@gmail.com>
2018-01-25 23:17:09 +0530

    Revert "Commit added new lines."
    This reverts commit ce346f29bb329781a178472f25821828babb9ef2.

--- SeleniumRC/src/practice3_4_2017/Hellojava.java
+++ SeleniumRC/src/practice3_4_2017/Hellojava.java
@@ -3,9 +3,7 @@ package practice3_4_2017;
 public class Hellojava {
 
 	public static void main(String[] args) {
-		int a,c=1,d=2;
-		a=c+d;
-		System.out.println(a);
+		// TODO Auto-generated method stub
 
 	}
 
